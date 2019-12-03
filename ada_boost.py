e80245e1a3e3dbb2e3a72c575a5674fc38aca878
avashist <avashist98@tamu.edu>
2019-12-03 13:01:41 -0600

    roundone

--- ada_boost.py
+++ ada_boost.py
@@ -4,15 +4,15 @@ import numpy as np
 import os 
 import time
 import matplotlib.pyplot as plt
-def alpha_cal(epsolon):
-    alpha = 0.5 * ln((1-epsolon)/epsolon)
-    return alpha
-def weight_cal(w,z,alpha,h,y):
-    w_new = w/z
-    return w_new
+def beta_cal(epsolon):
+    beta = 1/((1-epsolon)/epsolon)
+    return beta
+def weight_cal(Distribution,beta,label,prediction):
+    e = int(prediction == label)
+    Distribution_new = Distribution*beta**(1-e)
+    return Distribution_new
 
-def decision_stamp(S,Distribution)
-
+def decision_stamp(S,Distribution):
     col = []
     for names in S:
         col.append(str(names))
@@ -21,6 +21,7 @@ def decision_stamp(S,Distribution)
     S = S.merge(pd.Series(Distribution).to_frame(), left_index=True, right_index=True)
     S.columns = col 
     F_star = float('inf')
+    X  = S.drop(columns = ['Label','Distribution'])
     for col in X.columns:
         X  = S.drop(columns = ['Label','Distribution'])
         Xj = S.sort_values(by = col)
@@ -40,11 +41,24 @@ def decision_stamp(S,Distribution)
                 theta_star= 0.5*((Xj.iloc[i] + Xj.iloc[i+1])[0])
                 j_star=col
     return(j_star,theta_star)
+
+def error_calcuator(kind,S,threshold,J):
+    prediction = int(S[J] >= threshold)
+    label = S['Label']
+    if (kind == 1):
+        error = sum(prediction != label)/len(label)
+    elif (kind == 2):
+        error_2 =  sum((prediction == -1)& (label == 1))/sum(label == 1)
+    else:
+        error_3 =  sum((prediction == 1)& (label == -1))/sum(label == -1)
+    return prediction,error,error_2,error_3
+
 def ada_boost(S,rounds):
+
     alpha = np.array([0]*round)
     e_t = np.round([0]*round)
     for i in rounds:
-        
+        print(i)
     return(12)
 base_path  =  os.getcwd()
 train_df = pd.read_csv((base_path+'/Data/train_data.csv'),header = None)
@@ -56,5 +70,9 @@ test_y = train_df[test_df.columns[-1]]
 P_count = train_y[train_y == 1].count() 
 N_count = len(train_y) - P_count
 Distribution = np.array([1/(2*P_count)]*P_count + [1/(2*N_count)]*N_count)
-
+t0 = time.time()
+[j_star, theta_star] =  decision_stamp(train_X,Distribution)
+t1 = time.time()
+print(j_star,theta_star)
 print('complete')
+print(t1-t0)
