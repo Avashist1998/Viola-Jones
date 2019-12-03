e55a14f4ba35cf41b1eddefac343bf10131a3b72
avashist <avashist98@tamu.edu>
2019-12-02 23:45:38 -0600

    update

--- ada_boost.py
+++ ada_boost.py
@@ -1,1 +1,9 @@
 # the goal of the file is to develop the ada_boost algorithm 
+import pandas as pd 
+import numpy as np 
+import os 
+import time
+import matplotlib.pyplot as plt
+base_path  =  os.getcwd()
+train_df = pd.read_csv((base_path+'/Data/train_data.csv',header=None)
+test_df = pd.read_csv((base_path+'/Data/test_data.csv',header=None)
