5b3813ee4f6f0387aaf7e24fbb5b41000241ce21
avashist1998 <abhay.vashist1998@gmail.com>
2023-02-27 23:54:34 -0600

    fix: git lfs removal

--- README.md
+++ README.md
@@ -1,9 +1,9 @@
 [Paper](https://www.cs.cmu.edu/~efros/courses/LBMV07/Papers/viola-cvpr-01.pdf) 
 
 # Viola-Jones_Algorithm
-This is an implementation of the Viola-Jones Algorithm in python3. The results of the original algorithm are improved using Adaboost algorithm.
+This is an implementation of the Viola-Jones Algorithm in python3. The results of the original algorithm are improved using AdaBoost algorithm.
 
-# Enviornment Setup
+# Environment Setup
 
 > pip install -r requirements.txt
 
@@ -11,14 +11,14 @@ This is an implementation of the Viola-Jones Algorithm in python3. The results o
 
 If someone is running the code on a windows machine. They will have to change the path that is used in all of the file because the program was written a Unix based machine. The program makes used of parallelization to decrease execution time, so when running on single core machine on may observe longer execution times than stated.  
 
-# Accessing Data Set
+# Creating Dataset
 
-One can download the data set from git by running git lfs. Please refer to git lfs documentation. If one can on download the feature file, one can run the Haar_feature.py to extract their own feature file. 
+`python3 grad_project/dataset_generation.py -d dataset/ -o .dataset/`
 
 # Files Information
 
-Main.py used to plot the error extracting the features 
-Displaying_features.py used to display the Haar features on a default image
-ada_boost.py perform the AdaBoost algorithm on the data and outputs the best features and their respective threshold to a file
-Modified_Ada_boost.py same as AdaBoost but allow the user to place a gamma parameter to change the criteria.
+`main.py` used to plot the error extracting the features 
+`displaying_features.py` used to display the Haar features on a default image
+`ada_boost.py` perform the AdaBoost algorithm on the data and outputs the best features and their respective threshold to a file
+`Modified_Ada_boost.py` AdaBoost implementation with gamma parameter modification enabled criteria.
 
--- grad_project/utils/feature_extraction.py
+++ grad_project/utils/feature_extraction.py
@@ -1,17 +1,12 @@
 # The goal of the file to perform the feature extraction using the Haar_Features. 
 # I hoping to define the function in this file and worry about the rest later.
-import cv2
-import os
-import glob 
-import numpy as np 
-import matplotlib.pyplot as plt
+import numpy as np
 from typing import List
-import pandas as pd 
-import time
+
 # Setting up a assigning the label to the images
 # get the base path of the directory
 def integral_image(image: np.ndarray) -> np.ndarray:
-    [row,col] = image.shape
+    [row, col] = image.shape
     i_image: np.ndarray = image.copy()
     for i in range(0,row):
         for j in range(0,col):
@@ -29,7 +24,7 @@ def feature_extraction(image) -> List[float]:
                 for j in range(col-2*w+1):
                     output = -2*image_copy[i+h-1,j+w-1] + 2*image_copy[i,j+w-1] + image_copy[i+h-1,j+2*w-1] + image_copy[i+h-1,j] + image_copy[i,j+2*w-1] - image_copy[i,j]
                     feature.append(output)
-    print(len(feature))
+    # print(len(feature))
     #Type 2 features (two horizontal)
     for h in range(1,5):
         for w in range(1,9):
@@ -37,15 +32,15 @@ def feature_extraction(image) -> List[float]:
                 for j in range(col-w+1):
                     output = 2*image_copy[i+h-1,j] + image_copy[i+2*h-1,j+w-1] + image_copy[i,j+w-1] - 2*image_copy[i+h-1,j+w-1] - image_copy[i+2*h-1,j] - image_copy[i,j]
                     feature.append(output)
-    print(len(feature))
-    # Type 3 feature (three Horizonatal)
+    # print(len(feature))
+    # Type 3 feature (three Horizontal)
     for h in range(1,3):
         for w in range(1,9):
             for i in range(row-4*h+1): 
                 for j in range(col-w+1):
                     output = 2*image_copy[i+3*h-1,j+w-1] + 2*image_copy[i+h-1,j] - 2*image_copy[i+h-1,j+w-1] - 2*image_copy[i+3*h-1,j] - image_copy[i+4*h-1,j+w-1] + image_copy[i+4*h-1,j] - image_copy[i,j] + image_copy[i,j+w-1]
                     feature.append(output)
-    print(len(feature))
+    # print(len(feature))
     # Type 4 feature (two Vertical)
     for h in range(1,9):
         for w in range(1,3):
@@ -53,7 +48,7 @@ def feature_extraction(image) -> List[float]:
                 for j in range(col-4*w+1):
                     output = 2*image_copy[i,j+w-1] + 2*image_copy[i+h-1,j+3*w-1] - 2*image_copy[i,j+3*w-1] - 2*image_copy[i+h-1,j+w-1] - image_copy[i,j]+ image_copy[i+h-1,j] - image_copy[i+h-1,j+4*w-1] + image_copy[i,j+4*w-1]
                     feature.append(output)
-    print(len(feature))
+    # print(len(feature))
     # Type 5 feature (four)
     for h in range(1,5):
         for w in range(1,5):
@@ -61,78 +56,6 @@ def feature_extraction(image) -> List[float]:
                 for j in range(col-2*w+1):
                     output = image_copy[i,j]+ 4*image_copy[i+h-1,j+w-1] - 2*image_copy[i,j+w-1] - 2*image_copy[i+h-1,j] + image[i+2*h-1,j+2*w-1] - 2*image_copy[i+h-1,j+2*w-1] + image_copy[i,j+2*w-1] - 2*image_copy[i+2*h-1,j+w-1] + image_copy[i+2*h-1,j]
                     feature.append(output)
-    print(len(feature))
+    # print(len(feature))
     
     return feature
-
-#---------------------------------------------------------------------------------------------------------------------
-
-base_path  =  os.getcwd()
-train_faces_files = glob.glob(base_path+ '/dataset/trainset/faces/*.png')
-train_faces_files.sort()
-train_non_faces_files = glob.glob(base_path+ '/dataset/trainset/non-faces/*.png')
-train_non_faces_files.sort()
-data = np.array([[]])
-t0 = time.time()
-for names in train_faces_files:
-    image = cv2.imread(names,cv2.IMREAD_GRAYSCALE)
-    i_image = integral_image(image)
-    f = feature_extraction(i_image)
-    data = np.append(data,f)
-
-num_image = len(train_faces_files)
-num_feature = int(len(data)/num_image)
-data = np.resize(data, (num_image,num_feature))
-temp_data = np.array([[]])
-for names in train_non_faces_files:
-    image = cv2.imread(names,cv2.IMREAD_GRAYSCALE)
-    i_image = integral_image(image)
-    f = feature_extraction(i_image)
-    temp_data = np.append(temp_data,f)
-num_image = int(len(temp_data)/num_feature)
-temp_data = np.resize(temp_data, (num_image,num_feature))
-label = [1]*len(train_faces_files)
-label_non_faces = [-1] * len(train_non_faces_files)
-label = np.append(label,label_non_faces)
-total_data = np.concatenate((data,temp_data),axis=0)
-final = np.insert(total_data, num_feature ,label,axis=1)
-pd.DataFrame((final).astype(int)).to_csv(base_path+ "/train_data.csv",header=None, index=None,float_format= '%10.5f')
-t1 = time.time()
-print((t1-t0)/60)
-
-#---------------------------------------------------------------------------------------------------------------------
-# test file 
-
-train_faces_files = glob.glob(base_path+ '/dataset/testset/faces/*.png')
-train_faces_files.sort()
-train_non_faces_files = glob.glob(base_path+ '/dataset/testset/non-faces/*.png')
-train_non_faces_files.sort()
-data = np.array([[]])
-t0 = time.time()
-for names in train_faces_files:
-    image = cv2.imread(names,cv2.IMREAD_GRAYSCALE)
-    i_image = integral_image(image)
-    f = feature_extraction(i_image)
-    data = np.append(data,f)
-
-num_image = len(train_faces_files)
-num_feature = int(len(data)/num_image)
-data = np.resize(data, (num_image,num_feature))
-temp_data = np.array([[]])
-for names in train_non_faces_files:
-    image = cv2.imread(names,cv2.IMREAD_GRAYSCALE)
-    i_image = integral_image(image)
-    f = feature_extraction(i_image)
-    temp_data = np.append(temp_data,f)
-num_image = int(len(temp_data)/num_feature)
-temp_data = np.resize(temp_data, (num_image,num_feature))
-label = [1]*len(train_faces_files)
-label_non_faces = [-1] * len(train_non_faces_files)
-label = np.append(label,label_non_faces)
-total_data = np.concatenate((data,temp_data),axis=0)
-final = np.insert(total_data, num_feature ,label,axis=1)
-#final.tofile(base_path + 'train_data.csv',sep=',',format='%10.5f')
-pd.DataFrame((final).astype(int)).to_csv(base_path + "/test_data.csv",header=None, index=None,float_format= '%10.5f')
-t1 = time.time()
-print((t1-t0)/60)
-#---------------------------------------------------------------------------------------------------------------------
