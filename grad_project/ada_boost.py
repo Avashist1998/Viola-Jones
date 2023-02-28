5867483b5d76c5f292d0e0842f3e64e3309f7089
avashist1998 <abhay.vashist1998@gmail.com>
2023-02-27 19:28:34 -0600

    fix: file movements

--- grad_project/ada_boost.py
+++ grad_project/ada_boost.py
@@ -6,13 +6,15 @@ import multiprocessing
 from joblib import Parallel, delayed
 
 t0 = time.time()
-def beta_cal(epsolon):
-    beta = 1/((1-epsolon)/epsolon)
+
+def beta_cal(epsilon: float) -> float:
+    beta = 1/((1-epsilon)/epsilon)
     return beta
+
 def weight_cal(Distribution,label,prediction):
     e = (prediction == label).astype(int)
-    epsolan = sum(Distribution*(1-e))
-    beta = beta_cal(epsolan)
+    epsilon: float = sum(Distribution*(1-e))
+    beta = beta_cal(epsilon)
     Distribution_new = Distribution*beta**e
     Distribution_new = Distribution_new/sum(Distribution_new)
     return Distribution_new,beta
@@ -36,7 +38,8 @@ def decision_stamp_search(list_data,row):
                 theta_star= 0.5*((Xj[i] + Xj[i+1]))
                 j_star=j
     return(j_star,theta_star)
-def parllel_sort(S_np,j,row):
+
+def parallel_sort(S_np,j,row):
     X_np  = S_np[:,:-2]
     Sort = S_np[:,j].argsort()
     Xj =  (S_np[:,j])[Sort]
@@ -52,11 +55,11 @@ def decision_stamp(S,Distribution):
     S_np[:,-1] = Distribution
     [row,col] = S_np.shape
     num_cores = multiprocessing.cpu_count()
-    processed_list = Parallel(n_jobs=num_cores,prefer = 'threads')(delayed(parllel_sort)(S_np,j,row) for j in range(col-2))
+    processed_list = Parallel(n_jobs=num_cores,prefer = 'threads')(delayed(parallel_sort)(S_np,j,row) for j in range(col-2))
     [j_star,theta_star] = decision_stamp_search(processed_list,row)
     return (j_star,theta_star)
 
-def error_calcuator(prediction,label):
+def error_calculator(prediction,label):
     error = sum(prediction != label)/len(label)
     error_2 =  sum((prediction == -1)& (label == 1))/len(label)
     error_3 =  sum((prediction == 1)& (label == -1))/len(label)
@@ -72,14 +75,14 @@ def ada_boost(S,y,rounds):
     for i in range(0,rounds):
         [J_star,theta_star] = decision_stamp(S,new_Distribution)
         prediction = 2*(S[S.columns[J_star]]>=theta_star).astype(int) - 1
-        [e1, e2,e3] = error_calcuator(prediction,y)
+        [e1, e2,e3] = error_calculator(prediction,y)
         if e1 <= 0.5:
             parity = 1
         else :
             parity = -1
             prediction = 2*(S[S.columns[J_star]]<=theta_star).astype(int) - 1
         parity_tol.append(parity)
-        [e1, e2,e3] = error_calcuator(prediction,y)
+        [e1, e2,e3] = error_calculator(prediction,y)
         [new_Distribution,beta] = weight_cal(new_Distribution,y,prediction,e1)
         beta_list.append(beta)
         j_of_round.append(J_star)
@@ -102,24 +105,24 @@ def df_maker(S):
     S = S.merge(pd.Series(Distribution).to_frame(), left_index=True, right_index=True)
     S.columns = col
     return S
+
 def total_error(test_X,test_y,theta,parity_tol):
     test_np_X = np.array(test_X)
     test_np_y = np.array(test_y)
     test_np_X[:,j_of_round[:]]
-    thresholed_total = (np.multiply(parity_tol,test_np_X[:,j_of_round[:]]) < np.multiply(1,theta)).astype(int)
+    threshold_total = (np.multiply(parity_tol,test_np_X[:,j_of_round[:]]) < np.multiply(1,theta)).astype(int)
     predictions_total = []
     combined_error = []
     alpha = np.log(1/np.array(beta_list))
     for i in range(1,len(alpha)+1):
-        prediction_t = (np.dot(thresholed_total[:,:i],alpha[:i]) >= 0.5*sum(alpha[:i])).astype(int)
+        prediction_t = (np.dot(threshold_total[:,:i],alpha[:i]) >= 0.5*sum(alpha[:i])).astype(int)
         predictions_total.append(prediction_t)
     predictions_total = np.array(predictions_total)
     sum_error = []
     for i in range(len(alpha)):
-        sum_error.append(error_calcuator(2*predictions_total[i]-1,test_np_y))
+        sum_error.append(error_calculator(2*predictions_total[i]-1,test_np_y))
     sum_error = np.array(sum_error)
     return(sum_error)
-
 
 
 base_path  =  os.getcwd()
